8d1332c8a14811a8deff0ae812fe0f9dea5f0166
9Chinz <MR.Chinnapat@hotmail.com>
2021-10-07 17:34:50 +0700

    edit add some detail in print

--- week6/exercise/shopping_cart.py
+++ week6/exercise/shopping_cart.py
@@ -35,18 +35,18 @@ cart_1 = ShoppingCart(1)
 cart_1.add_book('b1', 2, 100)
 cart_1.add_book('b1', 3)
 cart_1.add_book('b2', 1, 103.5)
-print(f"1 {cart_1.books}")
+print(f"books in cart 1: {cart_1.books}")
 
 cart_1.delete_book('b2')
-print(f"1 {cart_1.books}")
+print(f"books in cart 1: {cart_1.books}")
 
 cart_2 = ShoppingCart(2)
 cart_2.add_book('b1', 1, 200)
 cart_2.add_book('b2', 3, 250)
 
-print(f" {cart_1.get_total()}")
+print(f"cart 1 total price: {cart_1.get_total()}")
 
-print(f"2 {cart_2.books}")
-print(f" {cart_2.get_total()}")
+print(f"books in cart 2: {cart_2.books}")
+print(f"cart 2 total price: {cart_2.get_total()}")
 
 print(cart_1 < cart_2)
